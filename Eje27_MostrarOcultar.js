f98516cd3b5cae7285226c1cf0e29dd0183adfe5
alfercar <alfonsofdez97@gmail.com>
2022-02-14 19:47:32 +0100

    js

--- Eje27_MostrarOcultar.js
+++ Eje27_MostrarOcultar.js
@@ -5,24 +5,24 @@ botonmas.addEventListener("click", textomas)
 botonmenos.addEventListener("click", textomenos)
 
 
-//Opción 1
-// function textomas (){
-//     let texto = document.getElementById("frase2");
-//     texto.classList.remove("textoNO");
-//     let botonito2 = document.getElementById("botonmenos");
-//     botonito2.classList.remove("textoNO");
-//     let botonito1 = document.getElementById("botonmas");
-//     botonito1.classList.add("textoNO");
-// }
+// Opción 1
+function textomas (){
+    let texto = document.getElementById("frase2");
+    texto.classList.remove("textoNO");
+    let botonito2 = document.getElementById("botonmenos");
+    botonito2.classList.remove("textoNO");
+    let botonito1 = document.getElementById("botonmas");
+    botonito1.classList.add("textoNO");
+}
 
-// function textomenos (){
-//     let texto = document.getElementById("frase2");
-//     texto.classList.add("textoNO");
-//     let botonito2 = document.getElementById("botonmenos");
-//     botonito2.classList.add("textoNO");
-//     let botonito1 = document.getElementById("botonmas");
-//     botonito1.classList.remove("textoNO");
-// }
+function textomenos (){
+    let texto = document.getElementById("frase2");
+    texto.classList.add("textoNO");
+    let botonito2 = document.getElementById("botonmenos");
+    botonito2.classList.add("textoNO");
+    let botonito1 = document.getElementById("botonmas");
+    botonito1.classList.remove("textoNO");
+}
 
 
 //Opción 2
@@ -36,16 +36,17 @@ botonmenos.addEventListener("click", textomenos)
 
 //Opcion 3
 
-function textomas (){
-    let parrafo2 = document.getElementById("frase2");
-    if(parrafo2.style.display === 'none'){
-        parrafo2.style.display = 'block';
-        document.getElementById('botonmas').innerHTML = "Menos"
-    }
-    else {
-        parrafo2.style.display = 'none';
-        document.getElementById('botonmas').innerHTML = "Mas"
-    }
+// botonmas.addEventListener("click", textomas)
+// function textomas (){
+//     let parrafo2 = document.getElementById("frase2");
+//     if(parrafo2.style.display === 'none'){
+//         parrafo2.style.display = 'block';
+//         document.getElementById('botonmas').innerHTML = "Menos"
+//     }
+//     else {
+//         parrafo2.style.display = 'none';
+//         document.getElementById('botonmas').innerHTML = "Mas"
+//     }
 
-}
+// }
 
